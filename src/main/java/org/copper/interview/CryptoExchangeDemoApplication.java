ddbade8408cfe6321e1d9eb38dde296f35df9fa0
jonat <jonat@192.168.1.180>
2021-10-14 19:37:55 +0100

    git

--- src/main/java/org/copper/interview/CryptoExchangeDemoApplication.java
+++ src/main/java/org/copper/interview/CryptoExchangeDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CryptoExchangeDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CryptoExchangeDemoApplication.class, args);
+		 SpringApplication.run(CryptoExchangeDemoApplication.class, args);
 	}
 
 }
